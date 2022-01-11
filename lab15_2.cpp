5ad0644bfb5b4e5d011006b49e281f52593de8ae
Pitipat <pitipat_fu@cmu.ac.th>
2022-01-11 17:34:25 +0700

    commit lap15_2

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,39 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+void showMatrix(const bool Q[N][N]){
+	for(int i = 0 ; i < N ; i++ ){
+		for(int j = 0 ; j < N ; j++) {
+			cout << Q[i][j] << " " ;
+		}
+		cout << "\n" ;
+	}
+}
+
+void inputMatrix(double Q[N][N]){
+	for( int i = 0 ; i < N ; i++){
+		cout << "Row " << i+1 << ": " ;
+		for (int j = 0; j < N ; j++)
+		{
+			cin >> Q[i][j] ;
+		}
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N]){
+	for (int i = 0 ; i < N ; i++)
+	{
+		for (int j =  0 ; j < N ; j++ )
+		{
+			if (i == 0 || j == 0 ||  j == N-1 || i == N-1 ){
+			B[i][j] = 0 ;
+			continue ;
+			}
+			if(A[i][j] >= A[i-1][j] && A[i][j] >= A[i+1][j] && A[i][j] >= A[i][j-1] && A[i][j] >= A[i][j+1] ){
+				B[i][j] = 1 ;
+			}else B[i][j] = 0 ; 
+		}
+		
+	}
+	
+}
